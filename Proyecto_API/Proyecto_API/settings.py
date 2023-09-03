43c5509e70ce43b793bd0d61bbf45dc42fa6db82
REDI <zkrtl0@gmail.com>
2023-09-03 13:31:28 -0600

    .

--- Proyecto_API/Proyecto_API/settings.py
+++ Proyecto_API/Proyecto_API/settings.py
@@ -83,8 +83,7 @@ DATABASES = {
         'HOST':'localhost',
         'PORT':'3306',
         'USER':'root',
-        #'PASSWORD':'Kuro1234@',
-        'PASSWORD':'afigth01',
+        'PASSWORD':'',
         'NAME': 'prueba_rh',
         'OPTIONS':{
             'init_command':"SET sql_mode='STRICT_TRANS_TABLES'"
